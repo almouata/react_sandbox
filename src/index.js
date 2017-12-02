d767c1558dbecca894096c31a5a67ce639e2014f
abdess <lmouatassime@gmail.com>
2017-12-02 23:03:48 +0000

    React using class component and functionnal componenet in addition with array.map()

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTsearch from 'youtube-api-search'
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
+const API_KEY = 'AIzaSyDFwyH6qaXUiqkePaEDqg1kCZvy4btQEpk';
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component{
+  constructor(props){
+    super(props);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    this.state = { videos : []}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    YTsearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos})
+    });
+  }
+  render(){
+    console.log("called render");
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
+}
+ReactDOM.render(<App/>, document.querySelector('.container'));
